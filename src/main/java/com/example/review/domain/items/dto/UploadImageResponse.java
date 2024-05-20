ab53d5c1aff9ef9735dbdf73623f9641b2e2d8ac
daye <jasmineldy@naver.com>
2024-05-20 23:25:21 +0900

    Fix: UploadImageResponse에서 imgUrls 삭제

--- src/main/java/com/example/review/domain/items/dto/UploadImageResponse.java
+++ src/main/java/com/example/review/domain/items/dto/UploadImageResponse.java
@@ -6,10 +6,8 @@ import java.util.List;
 @Schema(description = "상품 이미지 업로드 응답 DTO")
 public record UploadImageResponse(
     @Schema(description = "상품 이미지 id 리스트", example = "[1, 2, 3]")
-    List<Long> imgId,
+    List<Long> imgIds
 
-    @Schema(description = "상품 이미지 url 리스트", example = "[\"https://dachaebucket.s3.ap-northeast-2.amazonaws.com/123.jpg\"]")
-    List<String> imgUrls
 ) {
 
 }
